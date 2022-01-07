51ebeebfcf6daf0be0cd38361dbb250bdd89b6eb
tiana-horn <tiana.horn@gmail.com>
2022-01-07 15:25:55 -0500

    Add landing page

--- folade22/urls.py
+++ folade22/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from wedding import views as wedding_views
+from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
 urlpatterns = [
