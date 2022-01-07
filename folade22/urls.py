79c21aaf949b4d394752bff42f6d83554b1fe82c
tiana-horn <tiana.horn@gmail.com>
2022-01-07 14:37:23 -0500

    Add pages

--- folade22/urls.py
+++ folade22/urls.py
@@ -14,8 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from wedding import views as wedding_views
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', wedding_views.home, name='home'),
     path('admin/', admin.site.urls),
-]
+    path('gallery/', wedding_views.gallery, name='gallery'),
+    path('party/', wedding_views.party, name='party'),
+    path('registry/', wedding_views.registry, name='registry'),
+    path('rsvp/', wedding_views.rsvp, name='rsvp'),
+    path('schedule/', wedding_views.schedule, name='schedule'),
+    path('story/', wedding_views.story, name='story'),
+    path('travel/', wedding_views.travel, name='travel'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
