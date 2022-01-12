c2e9c771ffddb4695caa42772f5474a054426fa6
tiana-horn <tiana.horn@gmail.com>
2022-01-12 17:16:27 -0500

    heroku

--- folade22/settings.py
+++ folade22/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import django_heroku
 from django.contrib.messages import constants as messages
 from pathlib import Path
 import os
@@ -164,4 +165,6 @@ MESSAGE_TAGS = {
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'wedding.User'
+
+django_heroku.settings(locals())
 
