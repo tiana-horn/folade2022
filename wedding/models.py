429f8c9ba602df6686dbaf68dc161db4f3c56d7b
tiana-horn <tiana.horn@gmail.com>
2022-01-19 22:01:07 -0500

    Update database

--- wedding/models.py
+++ wedding/models.py
@@ -18,9 +18,11 @@ class Guest(models.Model):
 
 class Event(models.Model):
     name = models.CharField(max_length=222)
+    description = models.CharField(max_length=222,blank=True)
     date = models.DateField()
     time = models.CharField(max_length=222)
     location = models.CharField(max_length=777)
+    address = models.CharField(null=True,max_length=777)
     guests = models.ManyToManyField(Guest, through='Invitation')
 
     def __str__(self):
@@ -36,14 +38,29 @@ class Invitation(models.Model):
 
 class Accomodation(models.Model):
     title = models.CharField(max_length=222)
-    image = models.ImageField(upload_to='img', null=True)
-    image_alt_text = models.CharField(max_length=111)
-    location = models.CharField(max_length=777)
-    detail = models.TextField(max_length=777)
+    image = models.ImageField(upload_to='img', null=True, blank=True)
+    image_alt_text = models.CharField(max_length=111,blank=True)
+    location = models.CharField(max_length=777,blank=True)
+    phone = models.CharField(max_length=77,blank=True)
+    block_name = models.CharField(max_length=77,blank=True)
+    block_rate = models.CharField(max_length=77,blank=True)
+    detail = models.TextField(max_length=777,blank=True)
     link = models.URLField(unique=False, blank=True)
+    deadline = models.DateField(null=True,blank=True)
 
     def __str__(self):
         return self.title
+
+class Travel(models.Model):
+    location = models.CharField(max_length=222)
+    detail = models.TextField(max_length=1111,blank=True)
+    departure_time = models.TextField(max_length=1111,blank=True)
+    venue_distance = models.CharField(max_length=222,blank=True)
+    downtown_distance = models.CharField(max_length=222,blank=True)
+
+
+    def __str__(self):
+        return self.location
 
 class StoryText(models.Model):
     image1 = models.ImageField(upload_to='img', null=True)
@@ -56,7 +73,6 @@ class StoryText(models.Model):
     p2_part2 = models.TextField(max_length=1111)
     image2 = models.ImageField(upload_to='img', null=True)
     image2_alt_text = models.CharField(max_length=111)
-
 
     def clean(self):
         if StoryText.objects.exists() and not self.pk:
@@ -87,3 +103,21 @@ class GalleryImage(models.Model):
     
     def __str__(self):
         return self.image_alt_text
+
+class Host(models.Model):
+    image = models.ImageField(upload_to='img', null=True)
+    image_alt_text = models.CharField(max_length=111)
+    first_name = models.CharField(max_length=222)
+    last_name = models.CharField(max_length=222)
+    role = models.CharField(max_length=222)
+    description = models.TextField(max_length=777,blank=True)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class FAQ(models.Model):
+    question = models.TextField(max_length=1111)
+    answer = models.TextField(max_length=1111)
+    
+    def __str__(self):
+        return self.question
