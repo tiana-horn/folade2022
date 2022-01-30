1703637d2880a1273344c6d613f290a49869552f
tiana-horn <tiana.horn@gmail.com>
2022-01-30 03:32:42 -0500

    Add upload commands

--- folade22/settings.py
+++ folade22/settings.py
@@ -57,7 +57,7 @@ LOCKDOWN_PASSWORDS = ('test123','ladeatifola',)
 
 
 # SECURITY WARNING: don't run with debug set to True in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -78,6 +78,7 @@ INSTALLED_APPS = [
     #third party apps
     'lockdown',
     'storages',
+    'django_extensions'
 
 ]
 
---
+++ wedding/management/commands/load_guests.py
@@ -0,0 +1,37 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+import os.path
+import csv
+from wedding.models import Guest
+from django.core.files import File
+
+
+def get_path(file):
+    return os.path.join(settings.BASE_DIR, 'guests', file)
+
+class Command(BaseCommand):
+    help = "Load invitations onto site"
+
+    def add_arguments(self, parser):
+        # parser.add_argument('sample', nargs='+')
+        pass
+
+    def handle(self, *args, **options):
+        print('Deleting guests')
+        Guest.objects.all().delete()
+        with open(get_path('guests.csv'), 'r') as file:
+            reader = csv.DictReader(file)
+            i = 0
+            for row in reader:
+                i += 1
+                guest = Guest(
+                    name=row['name'],
+                    email=row['email'],
+                    aso_ebi=row['aso_ebi'],
+                    aso_ebi_paid=row['aso_ebi_paid'],
+                    hotel_accomodations=row['hotel_accomodations'],
+                    diet=row['diet'],
+                    food_allergies=row['food_allergies'],
+                )
+                guest.save()
+        print(f"{i} guestes loaded!")  
