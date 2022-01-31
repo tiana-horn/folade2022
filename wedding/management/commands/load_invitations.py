ef483615c8fbcb799dc53334a6febe60cf289700
tiana-horn <tiana.horn@gmail.com>
2022-01-30 20:21:52 -0500

    Delete existing invitations

--- wedding/management/commands/load_invitations.py
+++ wedding/management/commands/load_invitations.py
@@ -17,7 +17,8 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-
+		print(Deleting invitations)
+		Invitation.objects.delete.all()
         with open(get_path('invitations.csv'), 'r') as file:
             reader = csv.DictReader(file)
             i = 0
