b05132cef12ee733ee618f4c72503b15e508d02e
tiana-horn <tiana.horn@gmail.com>
2022-01-30 20:31:37 -0500

    Fix indentation

--- wedding/management/commands/load_invitations.py
+++ wedding/management/commands/load_invitations.py
@@ -17,8 +17,9 @@ class Command(BaseCommand):
         pass
 
     def handle(self, *args, **options):
-		print("Deleting invitations")
-		Invitation.objects.all().delete()
+		
+        print("Deleting invitations")
+	    Invitation.objects.all().delete()
         with open(get_path('invitations.csv'), 'r') as file:
             reader = csv.DictReader(file)
             i = 0
